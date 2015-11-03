f76ef07bb708d2636833e40ae1adf87cc1d482bd
genesiskoo <genesiskoo@gmail.com>
2015-11-03 19:18:06 +0900

    4th commit

--- movieApp.js
+++ movieApp.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'),
     logger = require('morgan');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/movies');
+
+var router = require('./router');
---
+++ router.js
@@ -0,0 +1,6 @@
+
+var express = require('express');
+var router = express.Router();
+
+var theater = require('./controllers/theater') ;
+var movies = require('./controllers/movies') ;
